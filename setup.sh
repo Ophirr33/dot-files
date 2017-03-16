c176f7c01b8036dc9f20c0165bbdfbfc5f2e9990
Ty <Ty_Coghlan@hms.harvard.dedu>
2017-03-16 17:41:40 -0400

    setup

---
+++ setup.sh
@@ -0,0 +1,5 @@
+cat canonrc.vim > ~/.vimrc
+cat tmux.conf > ~/.tmux.conf
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
